d3514c607357419a6f9474304a9e86995c8f32ed
abhisheksengupta <abhisheksengupta@192.168.1.72>
2019-02-12 21:54:46 +0800

    first commit

---
+++ LifeCycleManagementSystem/src/main/java/com/example/demo/LifeCycleManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeCycleManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeCycleManagementSystemApplication.class, args);
+	}
+
+}
+
